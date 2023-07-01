ecbfb716b9f56ea87afafdb76ec962e270a1b368
Truett Lee <truett129lee@gmail.com>
2023-07-01 14:39:42 -0500

    updated file to test deploy
    updated file to test deploy

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload (test3).
+          This is Truett Lee's app test
         </p>
         <a
           className="App-link"
